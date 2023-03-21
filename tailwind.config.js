7045535063642a6805cc723e9bcaaf3505fc19f4
meronshet <meri.wudie@gmail.com>
2023-03-21 14:02:25 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "logo":{
+            700:"#1C59AE",
+            800:"#114a9a",
+    },
+  },
+  plugins: [],
+}
+}
+}
